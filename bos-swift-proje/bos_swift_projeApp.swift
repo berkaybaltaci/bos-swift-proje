7ac4f1375109f7930ab0f621f1058b84e0e50c97
berkaybaltaci <berkaybaltaci@gmail.com>
2023-04-05 13:54:07 +0100

    Initial Commit

---
+++ bos-swift-proje/bos_swift_projeApp.swift
@@ -0,0 +1,17 @@
+//
+//  bos_swift_projeApp.swift
+//  bos-swift-proje
+//
+//  Created by Berkay Baltaci on 05/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct bos_swift_projeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
